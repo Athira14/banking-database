2efb1967eb06f7a604fb2dc38dbcb680f1a6da60
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:21:54 +0530

    modified the coding style and give comments

--- loan/loan_detail.py
+++ loan/loan_detail.py
@@ -1,7 +1,8 @@
+#To enter the loan_details into table lonan_details and display it
 import MySQLdb   
 
-db = MySQLdb.connect(host = "localhost", user = "root", passwd = "athira", 
-db = "bank") 
+db = MySQLdb.connect(host="localhost", user="root", passwd="athira", db="bank") 
+
 try:
     cursor = db.cursor()
     s ='y'
@@ -10,12 +11,15 @@ try:
         lname = raw_input("enter the name:")
         ltype = raw_input("enter the loan_type:")
         lamount = int(raw_input("Enter loan_amount:"))
-        cursor.execute("Insert into loan_details Values (%s,%s,%s,%s)",(lid,
-        lname,ltype,lamount))
+        cursor.execute(
+            "Insert into loan_details Values (%s,%s,%s,%s)", (lid,lname,ltype,lamount)
+        )
         s = raw_input("do u want conti(y/n)")
+
 except Exception as e:
     print e	
 db.commit()
+
 cursor.execute("select * from loan_details where loan_no = %d" %lid)
 for row in cursor.fetchall():
 	#print row
@@ -25,5 +29,6 @@ for row in cursor.fetchall():
     lamt = row[3]
     print "loan_no:%d,name:%s,loan_type:%s,loan_amount:%d"%(loan_no, name, ltype, lamt)
 
+cursor.close()
 	
 
