337194d73ac7ce22af06289e518372b3a8eb37d3
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:18:14 +0530

    give comments and change the styles

--- account/close_acc.py
+++ account/close_acc.py
@@ -1,17 +1,15 @@
 import MySQLdb   
-
-db = MySQLdb.connect(host="localhost", user="root", passwd="athira", 
-db="bank") 
+db = MySQLdb.connect(host = "localhost", user = "root", passwd = "athira", 
+db = "bank") 
 cursor = db.cursor()
-acc=int(raw_input("please enter the account_no"))
+acc = int(raw_input("please enter the account_no"))
 try:
-	cursor.execute("delete from withdrawal where account_no=%d" %acc);
-	cursor.execute("delete from account where account_no=%d" %acc);
+	cursor.execute("delete from withdrawal where account_no = %d" %acc);
+	cursor.execute("delete from account where account_no = %d" %acc);
 	db.commit();
 	print "Account deleted"
 
-except:
-	print "sorry this account doesn't exist"
+except Exception as e:
+	print str(e)	
 
-#for row in cursor.fetchall():
-	#print row,'\t\t'
+
