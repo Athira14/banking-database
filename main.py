101d7c80f3294de9b7c60045e7b92a5aa09588dd
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:28:41 +0530

    modified the coding style and give comments

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 
-#from account import account_list	  	
-s = int(raw_input("enter the choice 1.account 2.close account 3.withdrawal 4.loan 5.list account 6.list loan details 7.close loan"))
-if s == 1:
+ 	
+CHOICE = int(raw_input(
+	    "enter the choice 1.account 2.close account 3.withdrawal \
+        4.loan 5.list account 6.list loan details 7.close loan 8.add account_type")
+)
+if CHOICE == 1:
 	from account import account_details
-elif s == 2:
+elif CHOICE == 2:
 	from account import close_acc
 	from account import account_list
-elif s == 3:
+elif CHOICE == 3:
 	from withdraw import withdrawal
 	from withdraw import update
-elif s == 4:
+elif CHOICE == 4:
 	from loan import loan_detail
-elif s == 5:
+elif CHOICE == 5:
 	from account import account_list
-elif s == 6:
+elif CHOICE == 6:
 	from loan import loan_list
-else:
+elif CHOICE == 7:
 	from loan import close_loan
+else:
+	from account import account_type
