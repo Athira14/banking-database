d434873ebb6fcf52dc1454cf3043d81fd41d3db5
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:15:57 +0530

    give comments and change the styles

--- account/account_list.py
+++ account/account_list.py
@@ -1,9 +1,18 @@
+#To display the account details of every customers
 import MySQLdb   
-db = MySQLdb.connect(host="localhost", user="root", passwd="athira", 
-db="bank") 
+
+db = MySQLdb.connect(
+	    host="localhost", user="root", passwd="athira", db="bank"
+) 
 cursor = db.cursor()
-cursor.execute("select a.name, a.age, a.address, a.account_no, b.account_type, a.amount from account a, account_types b where a.acc_typeid = b.acc_typeid")
+#selecting the details from table account and account_types
+cursor.execute(
+	"select a.name, a.age, a.address, a.account_no, b.account_type, \
+    a.amount from account a, account_types b where a.acc_typeid = b.acc_typeid"
+)
+
 db.commit()
+
 for row in cursor.fetchall():
 	name = row[0]
 	age = row[1]
@@ -11,5 +20,6 @@ for row in cursor.fetchall():
 	acc = row[3]
 	acct = row[4]
 	amt = row[5]
-	print "name:%s, age:%d, address:%s, account_no:%d, account_type:%s, amount_deposit:%d"%(name, age, add, acc, acct, amt)
+	print "name: %s, age: %d, address: %s, account_no: %d, account_type: %s, \
+           amount_deposit: %d"%(name, age, add, acc, acct, amt)
 cursor.close()
