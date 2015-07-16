c1f10f936cf616d7d56376127b8eba33fde175ff
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:25:25 +0530

    modified the coding style and give comments

--- loan/close_loan.py
+++ loan/close_loan.py
@@ -1,12 +1,16 @@
+#To delete a loan_detail from loan_details
 import MySQLdb   
-db = MySQLdb.connect(host = "localhost", user = "root",passwd = "athira", 
-db = "bank") 
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="athira", db="bank") 
 cursor = db.cursor()
-loan_no = int(raw_input( "Enter loan_no: "))
+loan_no = int(raw_input("Enter loan_no: "))
+
 try:
-    cursor.execute("delete from loan_details where loan_no = %d"%loan_no)	
+    cursor.execute("delete from loan_details where loan_no = %d" % loan_no)	
     print "Loan closed"
+
 except:
     print "invalid loan_no"
+
 db.commit()
 cursor.close()
