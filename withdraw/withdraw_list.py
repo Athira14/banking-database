fd573d1ca55f2934452899b4ac534a608cbae8f1
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:27:15 +0530

    modified the coding style and give comments

--- withdraw/withdraw_list.py
+++ withdraw/withdraw_list.py
@@ -4,7 +4,6 @@ db = MySQLdb.connect(host = "localhost", user = "root",passwd = "athira",
 db = "bank") 
 cursor = db.cursor()
 from withdrawal import wcc
-#cursor.execute("select b.wno,a.name,a.age,a.address,a.account_no,a.account_type from account a,withdrawal b where a.account_no=b.account_no")
 cursor.execute("select b.wno,a.name ,a.age,a.address,a.account_no,a.account_type 
 from account a left join withdrawal b on a.account_no = %d"%wcc)
 
