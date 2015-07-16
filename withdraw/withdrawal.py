c3bf7b01135c21ffd989e917d0fd531f11a633f2
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:26:25 +0530

    modified the coding style and give comments

--- withdraw/withdrawal.py
+++ withdraw/withdrawal.py
@@ -1,16 +1,19 @@
+#To enter the withdrawal details
 import MySQLdb   
-db = MySQLdb.connect(host = "localhost", user = "root",passwd = "athira", 
-db = "bank") 
-s = 'y'
+db = MySQLdb.connect(host="localhost", user="root",passwd="athira", db="bank") 
+CHOICE = 'y'
+
 try:
     cursor = db.cursor()
-    while s == 'y':
+    while CHOICE == 'y':
         wid = int(raw_input( "Enter withdrawal_no:"))
         wcc = int(raw_input("enter the account_no"))
         wamount = int(raw_input("enter the withdrawal_amount"))
-        cursor.execute("Insert into withdrawal Values (%s, %s, %s)", (wid,wcc,wamount))
-        s = raw_input("do u want conti(y/n)")
+        cursor.execute("Insert into withdrawal Values (%s, %s, %s)", (wid, wcc, wamount))
+        CHOICE = raw_input("do u want conti(y/n)")
+
 except Exception as e:
     print e					
+
 db.commit()
 cursor.close()
