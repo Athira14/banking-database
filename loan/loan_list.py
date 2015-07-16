ce845a6949924a116370bfc7b649380de15c2bc6
Athira14 <athirabalachandran3332gmail.com>
2015-07-16 16:22:38 +0530

    modified the coding style and give comments

--- loan/loan_list.py
+++ loan/loan_list.py
@@ -1,13 +1,16 @@
+#To list the loan_details from table lonan_details
 import MySQLdb   
-db = MySQLdb.connect(host="localhost", user="root",passwd="athira", 
-db="bank") 
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="athira", db="bank") 
 cursor = db.cursor()
 cursor.execute("select * from loan_details group by loan_no")
 db.commit()
+
 for row in cursor.fetchall():
     no = row[0]
     name = row[1]
     loant = row[2]
     loanamt = row[3]
-    print "loan_no: %d, name: %s, loan_type: %s, loan_amount: %d"%(no, name, loant, loanamt)
+    print "loan_no: %d, name: %s, loan_type: %s, loan_amount: %d" %(no, name, loant, loanamt)
+
 cursor.close()
